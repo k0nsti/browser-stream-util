933efc8a26cb528a02620e25972dcb2a566b4e5c
Markus Felten <Markus.Felten@gmx.de>
2022-11-28 14:51:34 +0100

    feat: string2Stream()

---
+++ src/string2stream.mjs
@@ -0,0 +1,14 @@
+/**
+ * encodes a string into a stream.
+ * @param {string} str
+ * @param {TextEncoder} encoder
+ * @returns {ReadableStream}
+ */
+export function string2Stream(str, encoder = new TextEncoder()) {
+  return new ReadableStream({
+    async pull(controller) {
+      controller.enqueue(encoder.encode(str));
+      controller.close();
+    }
+  });
+}
